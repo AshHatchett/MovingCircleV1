483a9e055f6b733267a2fca796059c3e9e02785f
AshHatchett <98216571+AshHatchett@users.noreply.github.com>
2022-01-22 18:33:50 +0000

    Add files via upload

--- Moving Circle V1.cpp
+++ Moving Circle V1.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-#include <SFML>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main() {
-	cout << "Hello World!";
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
